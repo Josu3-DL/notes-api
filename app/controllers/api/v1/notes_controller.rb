01ab1c4b469d90d305da3c3a572104288f38c5cc
Puntitoa <josueduarte2131@gmail.com>
2025-08-17 15:32:54 -0600

    Complete RESTful API

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::NotesController < ApplicationController
+  before_action :set_note, only: [:show, :update, :destroy]
+
   def index
     notes = Note.all
     render json: { notes: notes }
@@ -7,14 +9,38 @@ class Api::V1::NotesController < ApplicationController
   def create
     note = Note.new(note_params)
     if note.save
-      render json: { note: note, status: :created, message: "Note created succesfully" }
+      render json: @note
     else
-      render json: { error: note.errors, status: :unprocesable_entity }
+      render json: { errors: @note.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show 
+    render json: @note 
+  end
+
+  def update
+    if @note.update(note_params)
+      render json: @note
+    else
+      render json: { error: @note.errors.full_messages, status: :unprocesable_entit }
+    end
+  end
+
+  def destroy
+    if @note.destroy
+      render json: {message: "Deleted succesfully"}
+     else
+      render json: { error: @note.errors.full_messages, status: :unprocesable_entit }
     end
   end
 
   private
   def note_params
     params.permit(:title, :content)
+  end
+
+  def set_note
+    @note = Note.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :notes, only: [:index, :create]
+      resources :notes
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
